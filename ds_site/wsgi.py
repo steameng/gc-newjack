cad167e51c10d02a1fafb37eda59d0864ebcce2b
mannowar <philip.manno@gmail.com>
2016-09-15 22:50:43 -0500

    Updated gitignore file to exclude local.py; Configured settings/producdtion.py file and wsgi.py file

--- ds_site/wsgi.py
+++ ds_site/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ds_site.settings")
 
 application = get_wsgi_application()
+
+try:
+    from dj_static import Cling
+    application = get_wsgi_application()
+except:
+    pass
