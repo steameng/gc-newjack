4e423f0b7e8bdc883967504d2afdc02a32a674f1
mannowar <philip.manno@gmail.com>
2016-09-15 21:31:53 -0500

    First commit; First Commit to github for creation

---
+++ ds_site/settings/__init__.py
@@ -0,0 +1,14 @@
+from .base import *
+
+'''Tries to import from local.py if the file exists. The file only should exist on local machine
+        and never the webserver. If the file does not exist, you are on the webserver and should
+        pull from the production.py file'''
+
+try:
+    from .local import *
+    InProduction = False
+except:
+    InProduction = True
+
+if InProduction:
+    from .production import *
