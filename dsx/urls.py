c18b0b969d86c380e491395b633dd12ef41a5d6c
mannowar <philip.manno@gmail.com>
2017-04-01 11:25:52 -0500

    changed more 1.8 patterns

--- dsx/urls.py
+++ dsx/urls.py
@@ -33,3 +33,10 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns += patterns('',
+#     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+# )
