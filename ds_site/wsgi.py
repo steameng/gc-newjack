c740472fdcef0fca02512905009b8defda20e23d
mannowar <philip.manno@gmail.com>
2017-04-01 10:37:09 -0500

    modifications for gc deployment

--- ds_site/wsgi.py
+++ ds_site/wsgi.py
@@ -15,8 +15,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ds_site.settings")
 
 application = get_wsgi_application()
 
-try:
-    from dj_static import Cling
-    application = get_wsgi_application()
-except:
-    pass
