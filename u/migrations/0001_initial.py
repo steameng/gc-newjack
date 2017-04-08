ae115dce8e12a1e7c41be6ba087a68a870179f04
mannowar <philip.manno@gmail.com>
2017-04-07 23:31:22 -0500

    REVERT; changed umedia model to filepath and not file obj

---
+++ u/migrations/0001_initial.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import u.models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UMedia',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('creation_date', models.DateTimeField(auto_now_add=True)),
+                ('song_file', models.FileField(upload_to=u.models.user_directory_path)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UMusic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('creation_date', models.DateTimeField(auto_now_add=True)),
+                ('last_updated', models.DateTimeField(auto_now=True)),
+                ('song_title', models.CharField(max_length=255, verbose_name=b'Song Title')),
+                ('song_json', models.TextField()),
+                ('song_seed', models.TextField()),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UPerson',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('creation_date', models.DateTimeField(auto_now_add=True)),
+                ('info', models.CharField(max_length=255, verbose_name=b'Song Title')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='umedia',
+            name='song',
+            field=models.ManyToManyField(to='u.UMusic'),
+        ),
+        migrations.AddField(
+            model_name='umedia',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+    ]
